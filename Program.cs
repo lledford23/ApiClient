82572e0b0d5cd8c5e047b9b990bcf6bc3a20eec9
Lindsay Ledford <lindsayledford0523@gmail.com>
2020-09-02 11:53:12 -0400

    finished project

--- Program.cs
+++ Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
 
 namespace ApiClient
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Joke
     {
-      Console.WriteLine("Welcome to C#");
+        public int id { get; set; }
+        public string type { get; set; }
+        public string setup { get; set; }
+        public string punchline { get; set; }
     }
-  }
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            var client = new HttpClient();
+
+            var responseAsStream = await client.GetStreamAsync("https://official-joke-api.appspot.com/random_joke");
+
+            var joke = await JsonSerializer.DeserializeAsync<Joke>(responseAsStream);
+
+            Console.WriteLine($"{joke.setup} - {joke.punchline}");
+
+        }
+    }
 }
